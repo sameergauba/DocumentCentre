90bf87f431e90a038331a4a37e01f46f13f09559
sameergauba <sameergauba94@gmail.com>
2017-09-25 23:11:05 +0530

    git ignore added

---
+++ src/main/java/com/ttn/geekcombat/SukhoiApplication.java
@@ -0,0 +1,12 @@
+package com.ttn.geekcombat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukhoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukhoiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ttn/geekcombat/SukhoiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ttn.geekcombat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SukhoiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
